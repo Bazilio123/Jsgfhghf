87f3fe2255986e415ca848e4be6999d70e8b3f62
Василь <bossvas@icloud.com>
2024-05-01 03:10:24 +0300

    dz 1  home gggggggggg

---
+++ Hoome js/Home 1.2.js
@@ -0,0 +1,9 @@
+let objVasil = {
+    firstname: "Vasil",
+    MiddleName: "Yaroslavovych",
+    lastName: "Shcherbay",
+}
+
+console.log(firstname);
+console.log(MiddleName);
+console.log(lastName);
---
+++ Hoome js/Home 1.3.js
@@ -0,0 +1,6 @@
+
+    let a= 100;
+    let b=100;
+    let c=true;
+
+console.log(a,b,c);

081013741e186835735b29f845bf488abbc3cda9
Василь <bossvas@icloud.com>
2024-05-12 02:23:11 +0300

    home 2

---
+++ Home js 2/home 2.2.js
@@ -0,0 +1,87 @@
+let BMW= [1,2,3,4,5,6,7,8,9,10];
+
+     console.log(BMW);
+
+let books
+
+      books={
+           title:"HarriPotter",
+           pageCount:"319",
+           genre:"fantasy",
+      };
+
+      books={
+          title:"the fourth wing",
+          pageCount:"528",
+          genre:"novel",
+      };
+
+      books={
+          title:"go where its  scary",
+          pageCount:"320",
+          genre:"adventures",
+
+      };
+      console.log(books);
+
+
+
+
+
+
+     let books
+
+           books={
+              title:"HarriPotter",
+              pageCount:"319",
+              genre:"fantasy",
+              authors:[name="K.Rowling",age=58],
+         };
+
+          books={
+              title:"the fourth wing",
+              pageCount:"528",
+              genre:"novel",
+              authors:[name="Rebecca Yarros",age=43],
+
+         };
+
+          books={
+              title:"go where its  scary",
+              pageCount:"320",
+              genre:"adventures",
+              authors:[name="Jim Loveless",age=56],
+
+          };
+console.log(books[name,age]);
+console.log(books[name,age]);
+console.log(books[name,age]);
+
+
+let interns1= [name="vasil",username="shcherbay",password=123];
+let interns2= [name="ivan",username="shcherbay",password=176];
+let interns3= [name="lesy",username="shcherbay",password=189];
+let interns4= [name="vika",username="shcherbay",password=222];
+let interns5= [name="nata",username="shcherbay",password=321];
+let interns6= [name="boddya",username="shcherbay",password=211];
+let interns7= [name="anna",username="shcherbay",password=140];
+let interns8= [name="nina",username="shcherbay",password=215];
+let interns9= [name="oxana",username="shcherbay",password=102];
+let interns10= [name="vira",username="shcherbay",password=819];
+let interns11= [name="nasty",username="shcherbay",password=963];
+
+
+console.log(interns1[password]);
+console.log(interns2[password]);
+console.log(interns3[password]);
+console.log(interns4[password]);
+console.log(interns5[password]);
+console.log(interns6[password]);
+console.log(interns7[password]);
+console.log(interns8[password]);
+console.log(interns9[password]);
+console.log(interns10[password]);
+console.log(interns11[password]);
+
+
+
